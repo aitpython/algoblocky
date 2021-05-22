f773aca1b30e1142e933de317d9b2aea918c5f92
Mohamed Badri <mohamed@nexiens.ma>
2021-05-22 20:06:19 +0100

    Init repo

---
+++ test-module.py
@@ -0,0 +1,22 @@
+from algoblocky import *
+
+point(5)
+avancer(50)
+
+couleur(colors.GREEN)
+point(5)
+orienter(directions.UP)
+avancer(50)
+
+couleur(colors.BLUE)
+point(5)
+orienter(directions.LEFT)
+avancer(50)
+
+couleur(colors.FUCSHIA)
+point(5)
+orienter(directions.DOWN)
+avancer(50)
+
+loop_forever()
+
